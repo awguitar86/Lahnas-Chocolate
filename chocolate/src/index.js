b619f2e87035349b98b48a1918169ca5e6282a9b
Austin Wright <awguitar86@gmail.com>
2018-03-29 16:34:24 -0600

    made  a ScrollToTop component, did some styling on products page for hovering over each products.

--- chocolate/src/index.js
+++ chocolate/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import './index.css';
 import App from './App';
+import ScrollToTop from './ScrollToTop';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-<BrowserRouter>
-    <App />
+<BrowserRouter onUpdate={() => window.scrollTo(0, 0)} history={createBrowserHistory()}>
+    <ScrollToTop>
+        <App />
+    </ScrollToTop>
 </BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
