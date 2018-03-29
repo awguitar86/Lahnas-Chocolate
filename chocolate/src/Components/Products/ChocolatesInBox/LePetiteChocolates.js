91a78ce724ed539b5833e9986f17c4adaed4213d
Austin Wright <awguitar86@gmail.com>
2018-03-28 22:38:09 -0600

    styled more of LePetite component and LePetiteChocolates component

--- chocolate/src/Components/Products/ChocolatesInBox/LePetiteChocolates.js
+++ chocolate/src/Components/Products/ChocolatesInBox/LePetiteChocolates.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
+import '../LePetite/lePetite.css';
 
 class LePetiteChocolates extends Component {
     render(){
         return(
             <div className='le-chocolates-in-box'>
-            <div className='le-chocolates-description'>
-                <h1>Chocolates in the Box</h1>
-                <p>
-                    Lahna’s hand-dipped chocolates are handcrafted with love and joy. If you’re not careful, you’ll eat all the chocolate you
-                    buy from her in one sitting. It’s so good you won’t want to give it as a gift, you’ll want to eat it all yourself! If you find
-                    yourself with a box of these amazing chocolates, make sure to enjoy every single bite, and then come back for more!
-                </p>
-            </div>
-            <div className='le-chocolates'>
-                <div>
-                    <div></div>
-                    <div></div>
+                <div className='le-chocolates-description'>
+                    <h1>Chocolates in the Box</h1>
+                    <p>
+                        Lahna’s hand-dipped chocolates are handcrafted with love and joy. If you’re not careful, you’ll eat all the chocolate you
+                        buy from her in one sitting. It’s so good you won’t want to give it as a gift, you’ll want to eat it all yourself! If you find
+                        yourself with a box of these amazing chocolates, make sure to enjoy every single bite, and then come back for more!
+                    </p>
                 </div>
-                <div></div>
+                <div className='le-chocolates'>
+                    <div className='top-chocolates'>
+                        <div className='chocolates'>
+                            <img />
+                            <h3>Sea Salt Caramel</h3>
+                            <p>
+                                Perfect caramel hand dipped in milk chocolate with a sprinkle of sea salt.
+                                Balanced perfection of sweet ’n salty!
+                            </p>
+                        </div>
+                        <div className='chocolates'>
+                            <img />
+                            <h3>Mint Truffle</h3>
+                            <p>
+                                The creamiest smooth chocolate mint centers dipped in pure milk chocolate.
+                                These may change life as you know it.
+                            </p>
+                        </div>
+                    </div>
+                    <div className='bottom-chocolates'>
+                        <div className='chocolates'>
+                            <img />
+                            <h3>Coconut Rough</h3>
+                            <p>
+                                Pure milk chocolate poured over pure coconut topped
+                                with a sprinkle of roasted coconut. A pure and lasting favorite.
+                            </p>
+                        </div>
+                        <div className='chocolates'>
+                            <img />
+                            <h3>Pecan Turtle</h3>
+                            <p>
+                                Pecans + caramel + chocolate = love!
+                                The ideal formula for yumminess.
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
         )
     }
 }
--- chocolate/src/Components/Products/LePetite/LePetite.js
+++ chocolate/src/Components/Products/LePetite/LePetite.js
@@ -4,6 +4,7 @@ import Footer from '../../Footer/Footer';
 import LePetiteChocolates from '../ChocolatesInBox/LePetiteChocolates';
 
 import LePetiteBox from '../../../images/lePetite.jpg';
+import './lePetite.css';
 
 class LePetite extends Component {
     render(){
@@ -14,19 +15,25 @@ class LePetite extends Component {
                     <div className='le-box'>
                         <div className='le-description'>
                             <h1>Le Petite Box</h1>
-                            <div className='le-add'></div>
+                            <div className='le-add'>
+                                <button>Add To Cart</button>
+                                <input placeholder='1'/>
+                                <h3>$4.95</h3>
+                            </div>
                             <p>
                                 Lahna’s Le Petite box of chocolates are the very best little gift to give to someone.
                                 It is the perfect amount of chocolate to help someone feel comfortaed and loved.
                                 This box conatins one piece each of the Sea Salt Caramel, Mint Truffle, Coconut Rough,
                                 and Pecan Turle.
                             </p>
-                            <h3>Ingredients</h3>
-                            <p>
-                                Love, joy, sweat, tears, marshmollow, chocolate, sometimes blood, roasted pecans, snot
-                                from grandkids, and coconut. Contains lots of sugar, milk, nuts, a the taste of
-                                guilt-free self indulgence. Gluten-free.
-                            </p>
+                            <div className='le-ingredients'>
+                                <h3>Ingredients</h3>
+                                <p>
+                                    Love, joy, sweat, tears, marshmollow, chocolate, sometimes blood, roasted pecans, snot
+                                    from grandkids, and coconut. Contains lots of sugar, milk, nuts, a the taste of
+                                    guilt-free self indulgence. Gluten-free.
+                                </p>
+                            </div>
                         </div>
                         <div className='le-picture'>
                             <img src={LePetiteBox} alt='box of chocolates'/>
