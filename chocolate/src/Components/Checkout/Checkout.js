451a45d8b887a4f5aae1460515b3b21b0286009a
Austin Wright <awguitar86@gmail.com>
2018-04-03 15:24:17 -0600

    Styled more of the Checkout component and shopping bag component.

--- chocolate/src/Components/Checkout/Checkout.js
+++ chocolate/src/Components/Checkout/Checkout.js
@@ -10,9 +10,59 @@ class Checkout extends Component {
             <div className='checkout-wrap'>
                 <Header />
                 <div className='checkout-body'>
-                    <h1>Items in Your Bag</h1>
-                    <div className='checkout-items'></div>
-                    <Link to='/checkout'><button>CHECKOUT</button></Link>
+                    <h1>Contact & Shipping Info</h1>
+                    <div className='checkout-form'>
+                        <input className='checkout-first' placeholder='First Name' />
+                        <input className='checkout-last' placeholder='Last Name'/>
+                        <input className='checkout-company' placeholder='Company(optional)'/>
+                        <input className='checkout-address' placeholder='Address'/>
+                        <input className='checkout-city' placeholder='City'/>
+                        <input className='checkout-state' placeholder='State' />
+                        <input className='checkout-zip' placeholder='Zip Code'/>
+                        <input className='checkout-phone' placeholder='Phone Number'/>
+                        <input className='checkout-email' placeholder='Email'/>
+                        <input className='checkout-reTypeEmail' placeholder='Re-Type Email'/>
+                    </div>
+
+                    <div className='payment-method'>
+                        <p>Payment Method:</p>
+                        <input type='radio' name='payment'/>
+                        <label for='paymentChoice1'>On Delivery</label>
+                        <input type='radio' name='payment'/>
+                        <label for='paymentChoice2'>Mail</label>
+                        <input type='radio' name='payment'/>
+                        <label for='paymentChoice3'>PayPal</label>
+                    </div>
+                    <div className='shipping-method'>
+                        <p>Shipping Method:</p>
+                        <input type='radio' name='shipping' value='' checked='checked'/>
+                        <label for='shippingChoice1'>Delivery Only</label>
+                    </div>
+
+                    <div className='total-price'>
+                        <div className='subtotal-shipping-tax'>
+                            <div className='subtotal'>
+                                <h3>Subtotal</h3>
+                                <p>$0.00</p>
+                            </div>
+                            <div className='shipping'>
+                                <h3>Shipping</h3>
+                                <p>$0.00</p>
+                            </div>
+                            <div className='tax'>
+                                <h3>Tax 6.5%</h3>
+                                <p>$0.00</p>
+                            </div>
+                        </div>
+                        <div className='price-divider'></div>
+                        <div className='total'>
+                            <h2>Total</h2>
+                            <p>$0.00</p>
+                        </div>
+                    </div>
+                    <div className='checkout-button'>
+                        <Link to='/checkout'><button>REVIEW ORDER</button></Link>
+                    </div>
                 </div>
                 <Footer />
             </div>
