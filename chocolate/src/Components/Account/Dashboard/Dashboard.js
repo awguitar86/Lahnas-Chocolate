49b17bbfacac84a06ec5609b7701205fe6898f43
Austin Wright <awguitar86@gmail.com>
2018-04-09 21:44:58 -0600

    wrote a componentDidMount method for connecting to database and setting state.

--- chocolate/src/Components/Account/Dashboard/Dashboard.js
+++ chocolate/src/Components/Account/Dashboard/Dashboard.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../../Header/Header';
 import Footer from '../../Footer/Footer';
-// import { findUserInfo } from '../../../services/account.services';
+import { findUserInfo } from '../../../services/account.services';
 import './dashboard.css';
 import { updateUser } from '../../../actions/actionCreators';
 import { connect } from 'react-redux';
@@ -11,56 +11,72 @@ class Dashboard extends Component {
     constructor(props){
         super(props);
         this.state = {
-            userInfo: {}
+            id: '',
+            first_name: '',
+            last_name: '',
+            company: '',
+            address: '',
+            city: '',
+            state: '',
+            zip_code: '',
+            phone: '',
+            email: '',
         }
         // this.handleGetUser = this.handleGetUser.bind(this);
     }
 
-    // handleGetUser(){
-    //     const id = this.props.match.params.id;
-    //     findUserInfo( id )
-    //     .then( res => res.data)
-    //     .catch( err => {throw err} );
-    // }
-
-    // if( res.status !== 200) {
-    //     alert(res);
-    // }
-    // else {
-    //     let userInfo = {
-    //         id: res.data[0].id,
-    //         first_name: res.data[0].first_name,
-    //         last_name: res.data[0].last_name,
-    //         company: res.data[0].company,
-    //         address: res.data[0].address,
-    //         city: res.data[0].city,
-    //         state: res.data[0].state,
-    //         zip_code: res.data[0].zip_code,
-    //         phone: res.data[0].phone,
-    //         email: res.data[0].email
-    //         }
-    //         this.props.updateUser(userInfo);
-    // }
-
+    componentDidMount(){
+        const id = this.props.match.params.id;
+        findUserInfo(id)
+        .then(res => {
+            if (res.status !== 200) {
+                alert(res);
+            }
+            else {
+                this.setState({
+                    id: res.data[0].id,
+                    first_name: res.data[0].first_name,
+                    last_name: res.data[0].last_name,
+                    company: res.data[0].company,
+                    address: res.data[0].address,
+                    city: res.data[0].city,
+                    state: res.data[0].state,
+                    zip_code: res.data[0].zip_code,
+                    phone: res.data[0].phone,
+                    email: res.data[0].email,
+                })
+            }
+        })
+        .catch( err => {throw err} )
+    }
 
     render(){
-        const userInfo = this.props.userInfo;
+        // const userInfo = this.props.userInfo;
+        let firstName = this.state.first_name;
+        let lastName = this.state.last_name;
+        let company = this.state.company;
+        let address = this.state.address;
+        let city = this.state.city;
+        let state = this.state.state;
+        let zipCode = this.state.zip_code;
+        let phone = this.state.phone;
+        let email = this.state.email;
         console.log(this.props.match.params.id);
         return(
             <div className='dash-wrap'>
                 <Header />
                 <div className='dash-body'>
                     <div className='dash-header'>
-                        Hello, {userInfo.first_name}!
+                        Hello, {firstName}!
                     </div>
                     <div className='dash-info'>
                         <ul className='dash-user-info'>
-                            <li className='dash-user1'>{userInfo.first_name} {userInfo.last_name}</li>
-                            <li className='dash-user2'>{userInfo.company}</li>
-                            <li className='dash-user3'>{userInfo.address}</li>
-                            <li className='dash-user4'>{userInfo.city}, {userInfo.state} {userInfo.zip_code}</li>
-                            <li className='dash-user5'>{userInfo.phone}</li>
-                            <li className='dash-user6'>{userInfo.email}</li>
+                            <li className='dash-user1'>{firstName} {lastName}</li>
+                            <li className='dash-user2'>{company}</li>
+                            <li className='dash-user3'>{address}</li>
+                            <li className='dash-user4'>{city}, {state} {zipCode}</li>
+                            <li className='dash-user5'>{phone}</li>
+                            <li className='dash-user6'>{email}</li>
                         </ul>
                         <ul className='dash-links'>
                             <Link to='/changeinfo'><button>Change Account Info</button></Link>
