20e02228661b02ca34833237adebfed50823f8da
Austin Wright <awguitar86@gmail.com>
2018-04-26 16:45:37 -0600

    changes

--- server/index.js
+++ server/index.js
@@ -59,8 +59,8 @@ passport.deserializeUser(function (user, done) {
 // ENDPOINTS
 //auth endpoint
 app.get('/login', passport.authenticate('auth0', {
-    successRedirect: "/dashboard",
-    failureRedirect: "/"
+    successRedirect: '/dashboard',
+    failureRedirect: '/'
 }))
 // check for logged in user
 app.get('/check', function (req, res) {
