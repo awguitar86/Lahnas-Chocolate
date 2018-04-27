ba8502d2e8dd782a094b4408dce48a3199212d48
Austin Wright <awguitar86@gmail.com>
2018-04-27 17:35:38 -0600

    Wrote a test nodemailer file. Changed some thing in contact router to test nodemailer there.

--- server/routers/contact.router.js
+++ server/routers/contact.router.js
@@ -11,31 +11,16 @@ contactMailer.post('/contact', (req, res, next) => {
             service: 'gmail',
             host: 'smtp.gmail.com',
             auth: {
-                    type: XOAuth2,
-                    clientId: process.env.GOOGLE_CLIENT_ID,
-                    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+                    XOAuth2: XOAuth2.createXOAuth2Generator({
+                        user: 'wright2896@gmail.com',
+                        clientId: '3205787395-42quhdu1biuc12glq3gjoab418li7joh.apps.googleusercontent.com',
+                        clientSecret: 'bGMrsk6-4tbTTPp_Juv00gTm',
+                        refreshToken: '1/kPpCygyu_bb7a5owM0n9rOuEZ_sIv-ysapPla7qn4whcjerR4NwRvUQYZO15MfQW',
+                    })
             },
     }));
 
-    // let mailOptions = {
-    //     from: `${email}`,
-    //     to: `wright2896@gmail.com`,
-    //     subject: `${subject}`,
-    //     html:`
-    //         <h4 style="margin:0px;">${firstName} ${lastName}</h4>
-    //         <h4 style="margin:0px;">${company}</h4>
-    //         <h4 style="margin:0px;">${email}</h4>
-    //         <p>${message}</p>
-    //     `,
-    //     auth: {
-    //         user: process.env.GMAIL_USER,
-    //         refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-    //         accessToken: process.env.GOOGLE_ACCESS_TOKEN,
-    //         expires: 3599
-    //     }
-    // };
-
-    transporter.sendMail({
+    let mailOptions = {
         from: `${email}`,
         to: `wright2896@gmail.com`,
         subject: `${subject}`,
@@ -44,25 +29,16 @@ contactMailer.post('/contact', (req, res, next) => {
             <h4 style="margin:0px;">${company}</h4>
             <h4 style="margin:0px;">${email}</h4>
             <p>${message}</p>
-        `,
-        auth: {
-            user: process.env.GMAIL_USER,
-            refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-            accessToken: process.env.GOOGLE_ACCESS_TOKEN,
-            expires: 3599
+        `
+    };
+
+    transporter.sendMail(mailOptions, (err, res) => {
+        if(err) {
+            console.log('Error');
+        } else {
+            console.log('Email Sent');
         }
     });
 })
 
 module.exports = contactMailer;
-
-
-
-
-// (mailOptions, (error, info) => {
-//     if(error) {
-//         console.error(error);
-//     }
-//     console.log('Message sent: %s');
-//     res.send('Message Sent')
-// })
