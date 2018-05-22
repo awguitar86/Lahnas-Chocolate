b7e25b9c77b367e9a45c14214a82c148ec5a1c20
Austin Wright <awguitar86@gmail.com>
2018-05-22 16:49:36 -0600

    syntax fix

--- src/Components/HamDropDown/HamDropDown.js
+++ src/Components/HamDropDown/HamDropDown.js
@@ -40,17 +40,17 @@ export default class HamDropdown extends Component {
                 <div onClick={this.handleClick.bind(this)} className={this.props.className}>
                     <img src={this.props.img} alt=''/>
                 </div>
-                    <div className={'backdrop ' + (this.state.open ? 'open' : '')} onClick={this.handleClick.bind(this)}/>
-                    {this.state.open &&
-                    <div className={`${this.props.className} ham-dropdown-menu`} >
-                        <div onClick={this.handleClick.bind(this)} className='ham-menu-items'><Link to='/products'>Products</Link></div>
-                        <div onClick={this.handleClick.bind(this)} className='ham-menu-items'><Link to='/about'>About</Link></div>
-                        <div onClick={this.handleClick.bind(this)} className='ham-menu-items'><Link to='/contact'>Contact</Link></div>
-                        <div onClick={this.handleClick.bind(this)} className={loggedIn ? 'ham-false' : 'ham-true'}><Link to='/register'>Register</Link></div>
-                        <div onClick={this.handleClick.bind(this)} className={loggedIn ? 'ham-false' : 'ham-true'}><a href={process.env.REACT_APP_LOGIN}>Login</a></div>
-                        <div onClick={this.handleClick.bind(this)} className={loggedIn ? 'ham-account-true' : 'ham-false'}><Link to='/dashboard'>Account</Link></div>
-                        <div onClick={this.handleClick.bind(this)} className={loggedIn ? 'ham-account-true' : 'ham-false'}><Link to='/logout' onClick={logout}>Logout</Link></div>
-                    </div>}
+                <div className={'backdrop ' + (this.state.open ? 'open' : '')} onClick={this.handleClick.bind(this)}/>
+                {this.state.open &&
+                <div className={`${this.props.className} ham-dropdown-menu`} >
+                    <div onClick={this.handleClick.bind(this)} className='ham-menu-items'><Link to='/products'>Products</Link></div>
+                    <div onClick={this.handleClick.bind(this)} className='ham-menu-items'><Link to='/about'>About</Link></div>
+                    <div onClick={this.handleClick.bind(this)} className='ham-menu-items'><Link to='/contact'>Contact</Link></div>
+                    <div onClick={this.handleClick.bind(this)} className={loggedIn ? 'ham-false' : 'ham-true'}><Link to='/register'>Register</Link></div>
+                    <div onClick={this.handleClick.bind(this)} className={loggedIn ? 'ham-false' : 'ham-true'}><a href={process.env.REACT_APP_LOGIN}>Login</a></div>
+                    <div onClick={this.handleClick.bind(this)} className={loggedIn ? 'ham-account-true' : 'ham-false'}><Link to='/dashboard'>Account</Link></div>
+                    <div onClick={this.handleClick.bind(this)} className={loggedIn ? 'ham-account-true' : 'ham-false'}><Link to='/logout' onClick={logout}>Logout</Link></div>
+                </div>}
             </div>
         )
     }
